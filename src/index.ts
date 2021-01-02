e611d9b0e2fcf6d4d5a98f836a422bbd42a273fd
clintfidel <clintfidel@gmail.com>
2021-01-02 20:54:21 +0100

    create post entity and migration

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
 import { MikroORM } from "@mikro-orm/core";
-import { __prod__ } from "./constants";
+// import { Post } from "./entities/Post";
+import mikroOrmConfig from './mikro-orm.config'
 
 const main = async () => {
-  const orm = await MikroORM.init({
-		dbName: 'test-data',
-		debug: !__prod__,
-		type: 'postgresql'
-	});
+  const orm = await MikroORM.init(mikroOrmConfig);
+  orm.getMigrator().up();
+  // const post = orm.em.create(Post, { title: 'my first create'});
+  // await orm.em.persistAndFlush(post);
+  // const posts = await orm.em.find(Post, {});
+  // console.log(posts)
 };
 
 main();
